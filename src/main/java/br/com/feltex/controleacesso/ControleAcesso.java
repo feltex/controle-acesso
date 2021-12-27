a2dea2b2818c31a4ca9cc0647c3a1ca73170a4f6
Andre Felix <andrefelix.br@gmail.com>
2021-12-27 17:24:51 +0000

    Versão inicial

---
+++ src/main/java/br/com/feltex/controleacesso/ControleAcesso.java
@@ -0,0 +1,13 @@
+package br.com.feltex.controleacesso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleAcesso {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleAcesso.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/feltex/controleacesso/ControleAcessoTests.java
@@ -0,0 +1,13 @@
+package br.com.feltex.controleacesso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleAcessoTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
